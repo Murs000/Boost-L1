13237066487a85c49f2aab71a30cdb900a8d4668
Murs000 <129412232+Murs000@users.noreply.github.com>
2024-07-24 20:26:20 +0400

    Add. Lesson3

---
+++ BoostL3/Lesson3.cs
@@ -0,0 +1,26 @@
+namespace BoostL3
+{
+    public static class Lesson3
+    {
+        public static void Task1()
+        {
+
+        }
+        public static void Task2()
+        {
+            
+        }
+        public static void Task3()
+        {
+            
+        }
+        public static void Task4()
+        {
+            
+        }
+        public static void Task5()
+        {
+            
+        }
+    }
+}
